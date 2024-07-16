a15709eefa6720969d8a39285115c7324ed38aa7
Tanisha Soma <98698825+Tanishasoma@users.noreply.github.com>
2024-07-16 12:30:32 +0530

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,12 @@
+function myage(){
+var age = prompt("What is your age")  
+var x = 100 - age
+console.log(x)
+
+var y = x*52
+var z = x*365 
+var c = x*12 
+
+console.log("  you are left with   "+y+ "  no of weeks  "+z+"  no of days  "+c+"  no of months ")
+}
+myage()
