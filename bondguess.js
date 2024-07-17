380b820d7af3657d7eb53910efbcfe2bc6ed2792
Tanisha Soma <98698825+Tanishasoma@users.noreply.github.com>
2024-07-17 12:30:01 +0530

    Create bondguess.js

---
+++ bondguess.js
@@ -0,0 +1,11 @@
+var B= prompt("ENTER THE BOY'S NAME")
+var G = prompt("ENTER THE GIRL'S NAME")
+
+var lovepercentage = Math.floor(Math.random()*100)+1
+if(lovepercentage<40){  
+
+alert("love calculation   "+ lovepercentage  +"%"+"your relationship is not going well")
+} else
+{
+alert("love calculation   "+ lovepercentage  +"%"+"your relationship is perfect")
+}
