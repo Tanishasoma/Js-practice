f9a42f8fda8f8c9a66a061ab7cec48d4596316ab
Tanisha Soma <98698825+Tanishasoma@users.noreply.github.com>
2024-07-17 12:17:45 +0530

    Create bondcal.js

---
+++ bondcal.js
@@ -0,0 +1,5 @@
+var B= prompt("ENTER THE BOY'S NAME")
+var G = prompt("ENTER THE GIRL'S NAME")
+
+var lovepercentage = Math.floor(Math.random()*100)+1
+alert("love calculation "+ lovepercentage+"%")
