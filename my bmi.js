5d9a81a1b2ccbd1b34c9b618bc2ac3e509259db8
Tanisha Soma <98698825+Tanishasoma@users.noreply.github.com>
2024-07-18 12:41:19 +0530

    Create my bmi.js

---
+++ my bmi.js
@@ -0,0 +1,17 @@
+var ram =95
+var height1 =1.88
+var sham = 85
+var height2 = 1.76
+var tanisha = 95
+var height3 =1.88
+
+ function bmi(ram,height1,sham,height2, tanisha,height3){
+var x = ram/ (height1*height1)
+var y=sham/(height2*height2)
+ var z = tanisha/(height2*height2)    
+
+console.log(x,y)
+console.log("my height is this "+x+ "my weight is   "+y)
+var compare = x>y
+
+     console.log(compare)
