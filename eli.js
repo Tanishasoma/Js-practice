c3ec1721c677d1144266dc58d808f7910e2364ed
Tanisha Soma <98698825+Tanishasoma@users.noreply.github.com>
2024-07-19 11:58:40 +0530

    Create eli.js

---
+++ eli.js
@@ -0,0 +1,10 @@
+var age = prompt("Enter your age")
+var eligibilty = 18
+var result = age- eligibilty
+if(age>18){
+    alert("You are eligible for the driving license")
+}
+else{
+    alert("You are not eligible for the driving license u have to wait for   " + result + "years")
+}
+
