9d2dc68925481a01547cfc1ba5ad6e8d05a6f962
Tanisha Soma <98698825+Tanishasoma@users.noreply.github.com>
2024-07-19 13:13:12 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,13 @@
+const new1 =["time" , "date" , "month"]
+console.log(new1)
+ var t = new1[0]
+console.log(t)
+
+var r = new1.length
+console.log(r)
+
+var s = new1.pop();
+console.log(s)
+
+var u = new1.push("seconds");
+console.log(u)
